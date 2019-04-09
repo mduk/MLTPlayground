7f1bea8f2a3735bb384c0bd37ae5d25ec8b1da9d
Daniel Kendell <daniel.kendell@gmail.com>
2019-04-09 21:16:02 +0100

    added camera-options.sh for both scpreview and screcord

---
+++ camera-options.sh
@@ -0,0 +1,7 @@
+declare -A camera_options=(
+            [power_line_frequency]=2 # 60Hz
+                      [focus_auto]=0 # Off
+                   [exposure_auto]=3 # Aperture Priority Mode
+          [exposure_auto_priority]=0 # Off
+  [white_balance_temperature_auto]=0 # Off
+)
