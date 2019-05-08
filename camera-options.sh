eded1e3ca9207d1c9bc62e25e392b5fb8747cb32
Daniel Kendell <daniel.kendell@gmail.com>
2019-05-08 21:53:04 +0100

    fix exposure

--- camera-options.sh
+++ camera-options.sh
@@ -1,7 +1,8 @@
 declare -A camera_options=(
             [power_line_frequency]=1 # 50Hz
                       [focus_auto]=0 # Off
-                   [exposure_auto]=3 # Aperture Priority Mode
+                   [exposure_auto]=1 # Manual
+               [exposure_absolute]=100
           [exposure_auto_priority]=0 # Off
   [white_balance_temperature_auto]=0 # Off
 )
